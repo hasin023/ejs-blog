1ffe0f6c953b25014ce98ec43b0ea63eee9b0565
Hasin <hasinmahtab.alvee@gmail.com>
2024-02-26 13:47:05 +0600

    fix: redirecting pages after register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,63 +1,64 @@
 const formidable = require('formidable');
 const pool = require('../config/db');
+const fs = require('fs').promises;
 
 
 class AuthController {
     registerPage = (req, res) => {
         return res.render('dashboard/register.ejs', {
-            title: 'Register'
+            title: 'Register',
+            error: ''
         });
     }
 
     registerUser = async (req, res) => {
         const form = new formidable.IncomingForm();
 
-        form.parse(req, async (err, fields, files) => {
+        try {
+            const { fields, files } = await new Promise((resolve, reject) => {
+                form.parse(req, (err, fields, files) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve({ fields, files });
+                    }
+                });
+            });
+
             const { email, password } = fields;
             const { image } = files;
 
-            const user = {
-                email,
-                password,
-                image: image ? image[0].originalFilename : null
-            };
+            let imageNewName = Date.now() + image[0].originalFilename;
 
             const emailCheckQuery = `SELECT COUNT(*) AS count FROM users WHERE email = '${email}'`;
+            const emailExistsResult = await pool.query(emailCheckQuery);
+            const emailExists = emailExistsResult.rows[0].count > 0;
 
-            pool.query(emailCheckQuery, (err, result) => {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'There was a problem checking the email',
-                        error: err
-                    });
-                }
-
-                const emailExists = result.rows[0].count > 0;
-
-                if (emailExists) {
-                    return res.status(400).json({
-                        message: 'Email already exists'
-                    });
-                }
-
-
-                const insertQuery = `INSERT INTO users (email, password, img_url) VALUES ('${user.email}', '${user.password}', '${user.image}')`;
-
-                pool.query(insertQuery, (err, result) => {
-                    if (err) {
-                        return res.status(500).json({
-                            message: 'There was a problem registering the user',
-                            error: err
-                        });
-                    }
-
-                    return res.status(200).json({
-                        message: 'User registered successfully'
-                    });
+            if (emailExists) {
+                return res.status(400).render('dashboard/register', {
+                    title: 'Register',
+                    error: 'Email already exists'
                 });
+            }
+
+            const insertQuery = `INSERT INTO users (email, password, img_url) VALUES ('${email}', '${password}', '${imageNewName}')`;
+            await pool.query(insertQuery);
+
+            const disPath = `${__dirname}/../views/assets/images/${imageNewName}`;
+            await fs.copyFile(image[0].filepath, disPath);
+
+            return res.status(200).redirect('/login');
+
+        } catch (error) {
+            return res.status(500).render('dashboard/error.ejs', {
+                title: 'Error',
+                message: 'Internal server error',
+                error: error
             });
-        });
+        }
+
     };
+
 
 
 }
--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const authRouter = require('./Routes/authRoute');
 const cors = require('cors');
 const path = require('path');
-const fs = require('fs');
-const pool = require('./config/db');
+// const fs = require('fs');
+const session = require('express-session');
+const flash = require('express-flash');
+const cookieParser = require('cookie-parser');
+
+// Port
+const PORT = process.env.PORT || 3000;
+
+// Routes
+const authRouter = require('./Routes/authRoute');
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cookieParser());
 
+// Session
+app.use(session({
+    secret: 'randomsecret2345voice55',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Flash
+app.use(flash());
+
+// Set Views
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -25,11 +44,15 @@ app.use('/', authRouter)
 
 app.get('/login', (req, res) => {
     return res.render('dashboard/login.ejs', {
-        title: 'Login'
+        title: 'Login',
+        user: {
+            email: 'asdf@gmail.com',
+            password: 'holysmokes'
+        }
     });
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port: 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
